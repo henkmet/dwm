d08bcd5ee6434edcb99cf510c3b1d52a7d854ae7
henkmet <henkmetselaar@gmail.com>
2022-03-18 22:52:14 +0800

    cleanup of no longer used programs

--- config.h
+++ config.h
@@ -45,9 +45,7 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating    isfloatsingle   monitor */
     {"Luakit",        NULL,       NULL,       1<<1,         0,           0,            -1 },
-    {"Ope",         NULL,       NULL,       1<<2,         0,           0,            -1 },
     {"st-256color",   NULL,       NULL,       1,            0,           1,            -1 },
-    {"Claws-mail",    NULL,       NULL,       1<<4,         0,           0,            -1 },
     {"Spotify",       NULL,       NULL,       1<<7,         0,           0,            -1 },
     {"TelegramDesktop",NULL,      NULL,       1<<6,         0,           0,            -1 },
     {"mpv",           NULL,       NULL,       1<<8,         0,           0,            -1 },
