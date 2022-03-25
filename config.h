b6c643b1a9ffcf30492d5854483bacfd51929663
henkmet <henkmetselaar@gmail.com>
2022-03-25 08:23:39 +0800

    dmenu colours

--- config.h
+++ config.h
@@ -79,7 +79,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *concertcmd[] = { "concertcmd", NULL };
 static const char *mpctoggle[] = { "mpdcontrol", NULL };
